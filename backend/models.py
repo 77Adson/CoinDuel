5731140235e0ebdf3081acc4008136d3413771e2
Skruuki <skruuki@gmail.com>
2026-01-28 17:46:05 +0100

    models.py

--- backend/models.py
+++ backend/models.py
@@ -6,14 +6,14 @@ db = SQLAlchemy()
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
-    score = db.Column(db.Float, nullable=False) # Zysk/Strata w % lub USD
-    asset = db.Column(db.String(20)) # Na czym grał (np. BTC)
+    score = db.Column(db.Float, nullable=False)
+    assets = db.Column(db.String(100))               # "BTC,ETH,XRP"
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def to_dict(self):
         return {
-            'username': self.username,
-            'score': self.score,
-            'asset': self.asset,
-            'date': self.created_at.strftime('%Y-%m-%d %H:%M')
+            "username": self.username,
+            "score": round(self.score, 2),
+            "assets": self.assets,
+            "date": self.created_at.strftime("%Y-%m-%d %H:%M")
         }
