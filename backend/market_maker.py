883c1b305fe3150bf39551ec2921ca2725d4d940
Skruuki <skruuki@gmail.com>
2026-01-28 17:45:18 +0100

    market_maker.py

--- backend/market_maker.py
+++ backend/market_maker.py
@@ -1,4 +1,3 @@
-# market_maker.py
 import pandas as pd
 import os
 import random
@@ -6,7 +5,6 @@ from datetime import datetime
 
 DATA_FOLDER = "data"
 GAME_LENGTH = 250
-
 
 def load_all_markets():
     seed = datetime.now().strftime("%d-%m-%Y")
@@ -19,11 +17,10 @@ def load_all_markets():
             continue
 
         coin = file.replace("_1h.csv", "")
-        path = os.path.join(DATA_FOLDER, file)
+        df = pd.read_csv(os.path.join(DATA_FOLDER, file))
 
-        df = pd.read_csv(path)
         if len(df) < GAME_LENGTH:
-            continue  # KLUCZOWE
+            continue
 
         df = df.rename(columns={
             "Datetime": "time",
@@ -33,16 +30,10 @@ def load_all_markets():
             "Close": "close"
         })
 
-        df["time"] = (
-            pd.to_datetime(df["time"], utc=True)
-            .astype("int64") // 10**9
-        )
+        df["time"] = pd.to_datetime(df["time"], utc=True).astype("int64") // 10**9
 
-        max_start = len(df) - GAME_LENGTH
-        start = random.randint(0, max_start)
-
+        start = random.randint(0, len(df) - GAME_LENGTH)
         markets[coin] = df.iloc[start:start + GAME_LENGTH].to_dict("records")
 
     random.seed()
     return markets
-
